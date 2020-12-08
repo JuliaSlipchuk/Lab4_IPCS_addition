c199eed53df9d7f8e6a22d5e4e35aaeb54cf5f00
JuliaSlipchuk <juliaolex@knu.ua>
2020-12-08 20:23:56 +0200

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Lab5_addition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Input = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Output = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Input,
+            this.Output});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 5;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(172, 593);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Input
+            // 
+            this.Input.HeaderText = "Input";
+            this.Input.MinimumWidth = 3;
+            this.Input.Name = "Input";
+            this.Input.ReadOnly = true;
+            this.Input.Width = 53;
+            // 
+            // Output
+            // 
+            this.Output.HeaderText = "Output";
+            this.Output.MinimumWidth = 3;
+            this.Output.Name = "Output";
+            this.Output.Width = 53;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(191, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(460, 592);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(658, 13);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(559, 592);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(606, 611);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 29);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Decrypt";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(561, 646);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 62);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "GO2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1229, 730);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Input;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Output;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5_addition
+{
+    public partial class Form1 : Form
+    {
+        public static Dictionary<char, char> letters = new Dictionary<char, char>() 
+        { 
+            { 'а', 'я' },
+            { 'б', 'ю' },
+            { 'в', 'ь' },
+            { 'г', 'щ' },
+            { 'ґ', 'ш' },
+            { 'д', 'ч' },
+            { 'е', 'ц' },
+            { 'є', 'х' },
+            { 'ж', 'ф' },
+            { 'з', 'у' },
+            { 'и', 'т' },
+            { 'і', 'с' },
+            { 'й', 'р' },
+            { 'ї', 'п' },
+            { 'к', 'о' },
+            { 'л', 'н' },
+            { 'м', 'м' },
+            { 'н', 'л' },
+            { 'о', 'к' },
+            { 'п', 'й' },
+            { 'р', 'ї' },
+            { 'с', 'і' },
+            { 'т', 'и' },
+            { 'у', 'з' },
+            { 'ф', 'ж' },
+            { 'х', 'є' },
+            { 'ц', 'е' },
+            { 'ч', 'д' },
+            { 'ш', 'ґ' },
+            { 'щ', 'г' },
+            { 'ь', 'в' },
+            { 'ю', 'б' },
+            { 'я', 'а' }
+        };
+        List<char> auxiliaryChars = new List<char>() { ' ', '.', ',', '-', '+', '-', '=', '*', '/', '@', '"',
+            '!', '?', '&', '^', '%', '(', ')', '{', '}', '[', ']', '<', '>' };
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillDataGridView();
+        }
+        private void FillDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+            foreach (var ch in letters)
+            {
+                dataGridView1.Rows.Add(ch.Key, ch.Value);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<string> to = new List<string>();
+            try
+            {
+                for (int i = 0; i < 33; i++)
+                {
+                    to.Add(dataGridView1.Rows[i].Cells[1].Value.ToString());
+                }
+                if (IsValid(to))
+                {
+                    if (checkBox1.Checked)
+                    {
+                        Decrypt();
+                    }
+                    else
+                    {
+                        Encrypt();
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        private bool IsValid(List<string> letters)
+        {
+            if (string.IsNullOrWhiteSpace(richTextBox1.Text))
+            {
+                MessageBox.Show("Please, enter text to richTextBox");
+                return false;
+            }
+            if (letters.Distinct().Count() < 33)
+            {
+                MessageBox.Show("You cannot replace several letters with one");
+                return false;
+            }
+            return true;
+        }
+
+        private void Encrypt()
+        {
+            string strWithDelAuxiliaryChars = DeleteAuxiliaryChars();
+            string result = "";
+            foreach(var ch in strWithDelAuxiliaryChars)
+            {
+                result += letters[ch];
+            }
+            richTextBox2.Text = result;
+        }
+
+        private void Decrypt()
+        {
+            string result = "";
+            foreach(var ch in richTextBox1.Text.ToLower())
+            {
+                result += letters.Where(c => c.Value == ch).Select(c => c.Key).FirstOrDefault();
+            }
+            richTextBox2.Text = result;
+        }
+
+        private string DeleteAuxiliaryChars()
+        {
+            string result = "";
+            foreach(var ch in richTextBox1.Text)
+            {
+                if (auxiliaryChars.Contains(ch))
+                    continue;
+                result += ch;
+            }
+            return result.ToLower();
+        }
+    }
+}
